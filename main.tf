4420eba891c20e9ddc07d42668031ace6bec2aad
Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>
2025-09-13 12:04:53 +0530

    Update main.tf
    Signed-off-by: Imran Ali Khan <114231155+NubeEra-ImranAli@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "tomcat_server" {
-  ami           = "ami-ami-0933f1385008d33c4"
+  ami           = "ami-0933f1385008d33c4"
   instance_type = "t2.micro"
   security_groups = ["sgalltraffic"]  # Reference the existing security group
   key_name      = "mujahed"
